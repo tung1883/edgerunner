e178a1d0e1de31e22bea93c6d8e785b8f59750aa
Nguyen Duc Tung <nguyenductung1883@gmail.com>
2023-02-06 10:20:00 +0700

    Donchian channel and Chaikin Money Flow

--- indicators.py
+++ indicators.py
@@ -9,3 +9,15 @@ def cci(high, low, close, period=20):
     ma = np.convolve(tp, np.ones(period)/period, mode='same')
     md = np.array([np.mean(np.abs(tp[max(0,i-period):i+1] - ma[i])) for i in range(len(tp))])
     return (tp - ma) / (0.015 * md + 1e-8)
+
+def donchian_channel(high, low, period=20):
+    upper = np.array([high[max(0,i-period):i+1].max() for i in range(len(high))])
+    lower = np.array([low[max(0,i-period):i+1].min()  for i in range(len(low))])
+    mid = (upper + lower) / 2
+    return upper, lower, mid
+
+def chaikin_money_flow(high, low, close, volume, period=20):
+    clv = ((close - low) - (high - close)) / (high - low + 1e-8)
+    mfv = clv * volume
+    cmf = np.convolve(mfv, np.ones(period), mode='same') / (np.convolve(volume, np.ones(period), mode='same') + 1e-8)
+    return cmf
