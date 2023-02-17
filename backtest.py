a169ab1f23042ffb09bc191daff42ef1721f0498
Nguyen Duc Tung <nguyenductung1883@gmail.com>
2023-02-17 14:45:00 +0700

    win/loss statistics and max consecutive losses

--- backtest.py
+++ backtest.py
@@ -12,3 +12,20 @@ def transaction_cost_pnl(returns, signal, cost_bps=10):
     trades = np.abs(np.diff(signal, prepend=0))
     cost = trades * cost_bps / 10000
     return returns - cost
+
+def max_consecutive_losses(pnl):
+    best, cur = 0, 0
+    for p in pnl:
+        cur = cur + 1 if p < 0 else 0
+        best = max(best, cur)
+    return best
+
+def win_loss_stats(pnl):
+    wins  = pnl[pnl > 0]
+    losses= pnl[pnl < 0]
+    return {
+        "win_rate": len(wins) / (len(pnl) + 1e-8),
+        "avg_win":  wins.mean() if len(wins)  else 0.0,
+        "avg_loss": losses.mean() if len(losses) else 0.0,
+        "profit_factor": wins.sum() / (-losses.sum() + 1e-8),
+    }
