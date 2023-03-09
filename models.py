21be8e2441522234780f86f1765b1d321558cb60
Nguyen Duc Tung <nguyenductung1883@gmail.com>
2023-03-09 14:00:00 +0700

    random forest — better than LR on out-of-sample

--- models.py
+++ models.py
@@ -9,4 +9,10 @@ def train_lr(X_train, y_train):
 
 def labels(df, horizon=1):
     return (df['Close'].shift(-horizon) > df['Close']).astype(int)
+from sklearn.ensemble import RandomForestClassifier
+
+def train_rf(X_train, y_train, n_estimators=100):
+    model = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
+    model.fit(X_train, y_train)
+    return model
 
