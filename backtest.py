c6812986b09bd3586fe486e04c3ccb998c6f3fe1
Nguyen Duc Tung <nguyenductung1883@gmail.com>
2023-04-24 15:00:00 +0700

    stop-loss and take-profit logic in backtest

--- backtest.py
+++ backtest.py
@@ -26,3 +26,30 @@ def sharpe(returns, rfr=0.0):
 def max_drawdown(equity):
     peak = equity.cummax()
     return ((equity - peak) / peak).min()
+
+def run_with_stops(df, signals, stop_pct=0.05, tp_pct=0.10):
+    from config import INITIAL_CAPITAL, COMMISSION
+    capital  = INITIAL_CAPITAL
+    position = 0
+    entry    = 0.0
+    portfolio = []
+    for date, sig in signals.items():
+        price = df.loc[date, 'Close']
+        if position > 0:
+            if price <= entry * (1 - stop_pct):
+                capital  += position * price * (1 - COMMISSION)
+                position  = 0
+            elif price >= entry * (1 + tp_pct):
+                capital  += position * price * (1 - COMMISSION)
+                position  = 0
+        if sig > 0 and position == 0:
+            position = capital / price
+            capital -= position * price * (1 + COMMISSION)
+            entry    = price
+        elif sig < 0 and position > 0:
+            capital += position * price * (1 - COMMISSION)
+            position = 0
+        portfolio.append(capital + position * price)
+    import pandas as pd
+    return pd.Series(portfolio, index=signals.index)
+
