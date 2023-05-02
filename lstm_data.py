88f65bbddf6546800db214f04af20a69340a9eec
Nguyen Duc Tung <nguyenductung1883@gmail.com>
2023-05-02 09:15:00 +0700

    LSTM data prep — sliding window sequences for time series

---
+++ lstm_data.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+
+def make_sequences(arr, seq_len=30):
+    X, y = [], []
+    for i in range(seq_len, len(arr)):
+        X.append(arr[i-seq_len:i])
+        y.append(arr[i])
+    return np.array(X), np.array(y)
+
+def scale(arr):
+    mn, mx = arr.min(), arr.max()
+    return (arr - mn) / (mx - mn + 1e-9), mn, mx
+
+def unscale(arr, mn, mx):
+    return arr * (mx - mn) + mn
+
