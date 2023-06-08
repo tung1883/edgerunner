96783430526eca2afe03a02a6f91509fa60fbfbd
Nguyen Duc Tung <nguyenductung1883@gmail.com>
2023-06-08 13:40:00 +0700

    Black-Litterman posterior return estimation

--- portfolio_opt.py
+++ portfolio_opt.py
@@ -16,3 +16,14 @@ def sharpe_weights(returns, rfr=0.0):
     w     = inv @ (mu - rfr)
     return w / w.sum()
 
+
+def black_litterman(prior_returns, cov, P, Q, omega=None, tau=0.05):
+    """Black-Litterman posterior estimate."""
+    n = len(prior_returns)
+    if omega is None:
+        omega = np.diag(np.diag(P @ (tau * cov) @ P.T))
+    M1 = np.linalg.inv(tau * cov)
+    M2 = P.T @ np.linalg.inv(omega) @ P
+    post_cov = np.linalg.inv(M1 + M2)
+    post_ret = post_cov @ (M1 @ prior_returns + P.T @ np.linalg.inv(omega) @ Q)
+    return post_ret, post_cov
