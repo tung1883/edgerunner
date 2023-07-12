40ad2042b285ae731c4051a74efce11e3ef4bef9
Nguyen Duc Tung <nguyenductung1883@gmail.com>
2023-07-12 13:30:00 +0700

    learning rate schedule and early stopping for LSTM

--- lstm_train.py
+++ lstm_train.py
@@ -22,3 +22,23 @@ def train(X, y, hidden=64, epochs=50, lr=0.001):
             print(f'epoch {ep:3d}  loss={losses[-1]:.5f}')
     return cell, losses
 
+
+def lr_schedule(epoch, warmup=10, decay=0.98):
+    if epoch < warmup:
+        return epoch / warmup
+    return decay ** (epoch - warmup)
+
+def train_with_early_stopping(model, X_tr, y_tr, X_val, y_val,
+                               max_epochs=200, patience=15, base_lr=0.001):
+    best_val, wait, best_w = np.inf, 0, None
+    for epoch in range(max_epochs):
+        lr = base_lr * lr_schedule(epoch)
+        loss = _train_epoch(model, X_tr, y_tr, lr)
+        val_loss = _eval(model, X_val, y_val)
+        if val_loss < best_val:
+            best_val, wait = val_loss, 0
+        else:
+            wait += 1
+            if wait >= patience:
+                break
+    return best_val
