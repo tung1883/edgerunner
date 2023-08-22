f8ba927a3fb73b078ae19d9a035bb47950de98b7
Nguyen Duc Tung <nguyenductung1883@gmail.com>
2023-08-22 15:30:00 +0700

    overfitting check — out-of-sample performance drops 40 percent, need regularisation

--- evaluate.py
+++ evaluate.py
@@ -7,3 +7,9 @@ def report(y_true, y_pred):
     print(f'Recall   : {recall_score(y_true, y_pred):.3f}')
     print(f'F1       : {f1_score(y_true, y_pred):.3f}')
 
+def in_vs_out(in_sample, out_sample):
+    ratio = out_sample / (in_sample + 1e-9)
+    print(f'In-sample  Sharpe: {in_sample:.3f}')
+    print(f'Out-sample Sharpe: {out_sample:.3f}')
+    print(f'Ratio: {ratio:.2f}  ({'good' if ratio > 0.6 else 'overfitting'})')
+
