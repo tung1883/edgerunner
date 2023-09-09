c9cc1ea13d2d8e50d43eb35038735ac5da286cf7
Nguyen Duc Tung <nguyenductung1883@gmail.com>
2023-09-09 12:30:00 +0700

    order book with market and limit order fill logic

--- orders.py
+++ orders.py
@@ -15,3 +15,25 @@ class Order:
             return True
         return (self.side == 'buy'  and price <= self.limit_price) or                (self.side == 'sell' and price >= self.limit_price)
 
+
+class OrderBook:
+    def __init__(self):
+        self.open_orders = []
+        self.filled = []
+
+    def submit(self, ticker, qty, order_type="market", limit_price=None):
+        order = {"ticker": ticker, "qty": qty, "type": order_type, "limit": limit_price, "status": "open"}
+        self.open_orders.append(order)
+        return len(self.open_orders) - 1
+
+    def fill(self, idx, fill_price):
+        o = self.open_orders[idx]
+        if o["type"] == "limit" and fill_price > o["limit"]:
+            return False
+        o["status"] = "filled"
+        o["fill_price"] = fill_price
+        self.filled.append(o)
+        return True
+
+    def cancel(self, idx):
+        self.open_orders[idx]["status"] = "cancelled"
