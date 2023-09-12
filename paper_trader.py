1dfa11a5de8e4f0bb08f6409ee5c2a40fe98b873
Nguyen Duc Tung <nguyenductung1883@gmail.com>
2023-09-12 14:30:00 +0700

    paper trading simulator — execute signals without real money

---
+++ paper_trader.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+from portfolio import Portfolio
+
+class PaperTrader:
+    def __init__(self, capital=10000):
+        self.portfolio = Portfolio(capital)
+        self.log = []
+
+    def on_signal(self, ticker, signal, price):
+        if signal > 0:
+            qty = int(self.portfolio.capital * 0.1 / price)
+            if self.portfolio.buy(ticker, price, qty):
+                self.log.append({'action': 'BUY', 'ticker': ticker,
+                                  'price': price, 'qty': qty})
+        elif signal < 0:
+            self.portfolio.sell(ticker, price)
+            self.log.append({'action': 'SELL', 'ticker': ticker, 'price': price})
+
