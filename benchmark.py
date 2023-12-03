e5bd0f07add0a49be964544e8715cd4f53d53430
Nguyen Duc Tung <nguyenductung1883@gmail.com>
2023-12-03 10:00:00 +0700

    model benchmarking table

--- benchmark.py
+++ benchmark.py
@@ -16,3 +16,19 @@ def vs_bh(equity, prices):
 # MSFT: Sharpe=0.91  MDD=-16.5%  vs BH Sharpe=0.80
 # TSLA: Sharpe=0.42  MDD=-31.0%  vs BH Sharpe=0.35  (volatile, hard to trade)
 
+
+def benchmark_models(model_factories, X_train, y_train, X_test, y_test):
+    results = {}
+    for name, factory in model_factories.items():
+        m = factory()
+        m.fit(X_train, y_train)
+        preds = m.predict(X_test)
+        acc = np.mean(preds == y_test)
+        results[name] = {"accuracy": acc}
+    return results
+
+def print_benchmark_table(results):
+    print(f"{'Model':<25} {'Accuracy':>10}")
+    print("-" * 38)
+    for name, r in sorted(results.items(), key=lambda x: -x[1]["accuracy"]):
+        print(f"{name:<25} {r['accuracy']:>10.4f}")
